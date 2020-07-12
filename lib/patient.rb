e88e27219252d0ce8fa1da56e88d821eeaa3f4fb
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-07-12 17:16:09 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -12,6 +12,14 @@ class Patient
     @@all
   end
   
+  def new_appointment(date, doctor)
+    Appointment.new(date, self, doctor)
+  end
   
+  def appointments
+    Appointment.all.select do |appt|
+      appt.doctor == self
+    end
+  end
   
 end
