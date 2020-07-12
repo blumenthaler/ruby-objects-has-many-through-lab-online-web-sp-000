bf952256217b64f885f16a70894ef6e4d0bf2e87
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-07-12 16:53:13 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,7 @@
 class Song
   attr_accessor :name, :artist, :genre
   
-  def initialize(name, artist, genre)
+  def initialize(name, artist = nil, genre)
     @name = name
     @artist = artist
     @genre = genre
