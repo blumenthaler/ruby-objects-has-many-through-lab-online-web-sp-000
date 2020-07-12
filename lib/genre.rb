c4bc1c9a87e30d297ab9fc73d57a5b92dada7aae
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-07-12 16:37:05 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -21,8 +21,7 @@ class Genre
   def artists
     # iterates over this genre's songs & collects the artist that owns each song
     songs.collect do |genres_song|
-      
-      
+      genres_song.artist
     end
   end
   
